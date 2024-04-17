1af795e13283b4a20d80f42755c048bdffc297e8
Kevin <92882620+Credible-Zebra@users.noreply.github.com>
2024-04-17 10:07:03 -0400

    Initial commit

---
+++ com/example/StringStuff.java
@@ -0,0 +1,26 @@
+
+/**
+ * Change me.
+ *
+ * @author Kevin Csiffary
+ * @version 1.0
+ * @since 2024-04-17
+ */
+
+// StringStuff class
+public final class StringStuff {
+
+    /** Private constructor to prevent instantiation. */
+    private StringStuff() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
